3da23db006acb718fa213e9a436dbba783685ddc
Alexey <brech_36@mail.ru>
2023-02-07 13:58:37 +0900

    add prog2

--- Program.cs
+++ Program.cs
@@ -3,18 +3,21 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,
 // 8 7,8 -7,1 9
+
 Console.Write("Введите количество строк: ");
-int m=Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
-int n=Convert.ToInt32(Console.ReadLine());
-double[,] num=new double[m,n];
-Random rand=new Random();
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] num = new double[m, n];
+Random rand = new Random();
 for (int i = 0; i < num.GetLength(0); i++)
 {
     for (int j = 0; j < num.GetLength(1); j++)
     {
-     num[i,j]= rand.Next(-10,10) + rand.NextDouble();
-     Console.Write($" {Math.Round (num[i,j],2)}");
+        num[i, j] = rand.Next(-10, 10) + rand.NextDouble();
+
+        Console.Write($" {Math.Round(num[i, j], 2)}");
     }
+
     Console.WriteLine();
 }
